3cf0265dcd5937cd75f314aaa97fe2e28cf9043f
kamil <kamil.wujec@woodpecker.co>
2017-11-01 23:00:18 +0100

    initial commit

---
+++ desktop/src/com/korba/gameoff/oblivious/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.korba.gameoff.oblivious.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.korba.gameoff.oblivious.ObscurityMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ObscurityMain(), config);
+	}
+}
---
+++ html/src/com/korba/gameoff/oblivious/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.korba.gameoff.oblivious.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.korba.gameoff.oblivious.ObscurityMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ObscurityMain();
+        }
+}
